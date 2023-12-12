439f3be25d0937a0f8b8d4f595b346d789b59679
Gabriel Santana <130417936+GabrielSantana013@users.noreply.github.com>
2023-12-12 09:19:41 -0300

    Create todolist.c
    Criação do arquivo inicial do projeto.

---
+++ todolist.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "ncurses.h"
+
+/*
+Projeto feito por: Gabriel Santana e Natanael Lima
+Data do Início: 12/12/2023
+Data do Término:
+*/
+
+
+int main(){
+
+
+
+  return 0;
+}
