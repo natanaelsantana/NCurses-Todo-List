42cb8f575755d78635cf0badeefb4f62d7c487db
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2023-12-12 19:08:26 -0300

    Esboço do programa curses

--- todolist.c
+++ todolist.c
@@ -6,14 +6,29 @@
 Projeto feito por: Gabriel Santana e Natanael Lima
 Data do Início: 12/12/2023
 Data do Término:
+
+  Para compilar e executar vá até o diretório correspondente e digite:
+
+  gcc teste.c -o testes.x -lncurses
+  ./testes.x
 */
 
+int main()
+{
 
-int main(){
-    initscr(); //inicia o modo curses
-    raw(); //teclas de controle não geram sinais de interrupção, diferente do cbreak() onde elas geram.
-    printw("Teste");
-    refresh(); //passa as informações que estavam no stdscr para a tela de fato (altera somente as atualizações)
-    endwin();  //finaliza o modo curses
+  int xmax, ymax;
+  initscr(); // inicia o modo curses
+  cbreak();     // teclas de controle geram sinais de interrupção, diferente do raw() onde elas n geram.
+  noecho();
+  getmaxyx(stdscr, ymax, xmax);
+
+  mvprintw((ymax/4), (xmax/2), "Insira a logo aqui");
+  printw("Fazer Cadastro");
+  printw("Feito por: Gabriel Santana e Natanael Lima");
+
+  refresh(); // passa as informações que estavam no stdscr para a tela de fato (altera somente as atualizações)
+  getch();
+  endwin(); // finaliza o modo curses
+
   return 0;
 }
