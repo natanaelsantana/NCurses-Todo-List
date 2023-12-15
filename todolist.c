2fe049f0407bbd8ce3118bd1e9c057556acbf33a
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2023-12-14 22:11:27 -0300

    Adicionado delimitação das opções do menu.

--- todolist.c
+++ todolist.c
@@ -17,16 +17,16 @@ int main()
 {
 
   int xmax, ymax;
-  initscr(); // inicia o modo curses
+  initscr();    // inicia o modo curses
   cbreak();     // teclas de controle geram sinais de interrupção, diferente do raw() onde elas n geram.
   noecho();
   getmaxyx(stdscr, ymax, xmax);
 
-  //printw("x: %d y: %d", xmax, ymax);
-
-  //newwin(7, (xmax - 6), (ymax - 9), 3);
-
-  WINDOW * janela = newwin(7, (xmax - 6), (ymax - 9), 3);
+    WINDOW * janela = newwin(4, 20, (ymax/2.1), (xmax/2.3)); // newwin(altura,largura (colunas), coordenada y em relação a stdscr, coordenada x em relaçãoa stdscr)
+    //mvwprintw(janela, (ymax) , (xmax), "zzz");  testar pq a janela não fecha
+    box(janela, 0, 0);
+    refresh();
+    wrefresh(janela);
 
   mvprintw((ymax/8), (xmax/2.25), "Insira a logo aqui");
   mvprintw((ymax/2),(xmax/2.25),"Fazer Cadastro\n");
@@ -36,7 +36,7 @@ int main()
 
 
   refresh(); // passa as informações que estavam no stdscr para a tela de fato (altera somente as atualizações)
-  getch();
+  getch();   //espera entrada do usuário
   endwin(); // finaliza o modo curses.
 
   return 0;
