e4236d6f8b2d38d86c6fc4d1b4997df2ffe18f85
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2023-12-27 23:28:12 -0300

    Criado a versão inicial da função AboutTheProject

--- AboutTheProject.c
+++ AboutTheProject.c
@@ -1,42 +1,100 @@
 #include "stdio.h"
 #include "ncurses.h"
+#include "Logos.h"
 
-const char *texto1 = "Gravidade: mede o impacto de um problema ou demanda\n"
-                     "para os demais processos de negocio.\n"
-                     "Se essa operacao nao for feita imediatamente, quais serao as consequencias?\n"
-                     "Os niveis podem ser:\n"
-                     "1 - Sem gravidade\n"
-                     "2 - Pouco grave\n"
-                     "3 - Grave\n"
-                     "4 - Muito grave\n"
-                     "5 - Extremamente grave\n";
+const char *texto1[] = {
+    "Gravidade: mede o impacto de um problema ou demanda",
+    "para os demais processos de negocio.",
+    "Se essa operacao nao for feita imediatamente,",
+    "quais serao as consequencias?",
+    "",
+    "Os niveis de Gravidade sao:",
+    "",
+    "1 - Sem gravidade",
+    "2 - Pouco grave",
+    "3 - Grave",
+    "4 - Muito grave",
+    "5 - Extremamente grave"};
 
-const char *texto2 = "";
+const char *texto2[] = {
+    "Urgencia: tente sempre responder se um problema pode esperar.",
+    "Em alguns casos, ele e extremamente importante,",
+    "mas nao precisa de solução imediata.",
+    "portanto, deve ser resolvido posteriormente.",
+    "",
+    "Os niveis de urgencia sao:",
+    "",
+    "1 - pode esperar",
+    "2 - pouco urgente",
+    "3 - urgente, deve ter atenção rapida",
+    "4 - muito urgente, deve ser a prioridade zero do momento",
+    "5 - urgentissimo e inadiavel, necessidade de acao imediata"};
 
-const char *texto3 = "";
+const char *texto3[] = {
+    "Tendência: avalia a tendência do problema se agravar",
+    "caso demore a ser solucionado e avalia se essa 'piora'",
+    "será brusca ou lenta.",
+    "",
+    "",
+    "Os niveis de Tendencia sao:",
+    "",
+    "1 - manterá a estabilidade",
+    "2 - deve piorar em longo prazo",
+    "3 - possibilidade de piorar em médio prazo",
+    "4 - possibilidade de piorar em curto prazo",
+    "5 - piora imediata",
+};
 
-int main(){
-
+void AboutTheProject()
+{
     initscr();
     cbreak();
     noecho();
 
-    int xmax, ymax;
+    int xmaxstdscr, ymaxstdscr;
 
-    getmaxyx(stdscr, ymax, xmax);
+    getmaxyx(stdscr, ymaxstdscr, xmaxstdscr);
 
-    WINDOW *janelaGravidade = newwin(10, 90, (ymax / 2.1), (xmax / 2.3));
-    box(janelaGravidade,0,0);
+    WINDOW *janelaGravidade = newwin(15, 54, (ymaxstdscr/2), (xmaxstdscr / 20));
+    //box(janelaGravidade, 0, 0);
     refresh();
 
-    //mvwprintw(stdscr,10,20,"teste");
-    mvwprintw(janelaGravidade,1,3, "%s", texto1);
-    
+    WINDOW *janelaUrgencia = newwin(14,64, (ymaxstdscr/2), (xmaxstdscr/3));
+    //box(janelaUrgencia,0,0);
+    refresh();
 
+    WINDOW *janelaTendencia= newwin(14,60, (ymaxstdscr/2), (xmaxstdscr/1.5));
+    //box(janelaTendencia, 0,0);
+    refresh();
+
+    WINDOW *janelaLogoAbt = newwin(15, 100, (ymaxstdscr / 8), (xmaxstdscr / 2.5));
+    refresh();
+
+    for (int i = 0; i < 12; i++)
+    {
+        mvwprintw(janelaGravidade, i + 1, 1, "%s", texto1[i]);
+    }
+
+    for (int i = 0; i < 12; i++)
+    {
+        mvwprintw(janelaUrgencia, i + 1, 1, "%s", texto2[i]);
+    }
+
+    for (int i = 0; i < 12; i++)
+    {
+        mvwprintw(janelaTendencia, i + 1, 1, "%s", texto3[i]);
+    }
+
+    wprintw(janelaLogoAbt, "%s", logoAboutTheProject);
+
+
+    wrefresh(janelaLogoAbt);
     wrefresh(janelaGravidade);
+    wrefresh(janelaUrgencia);
+    wrefresh(janelaTendencia);
     wrefresh(stdscr);
     getch();
     endwin();
 
-    return 0;
+    
 }
