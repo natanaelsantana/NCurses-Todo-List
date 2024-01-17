99c3f6614b3d70c65b26dcf3c7a6ed2567cf8ba8
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-01-16 22:16:40 -0300

    Adicionado versão provisória da interface gráfica

--- Register.c
+++ Register.c
@@ -1,5 +1,6 @@
 #include "stdio.h"
 #include "ncurses.h"
+#include "Logos.h"
 
 
 typedef struct user{
@@ -10,28 +11,42 @@ typedef struct user{
 
 }user;
 
-void cadastro()
+void cadastro(WINDOW *janelaLogo, WINDOW *janelaLogin)
 {
     int ch, i = 0;
     struct user user; //declara a variavel user do tipo struct user
 
-    mvprintw(10,10,"login:");
+    box(janelaLogin,0,0);
+
+    wprintw(janelaLogo, "%s", logoMenu);
+    wrefresh(janelaLogo);
+    refresh();
+
+    mvwprintw(janelaLogin,1,1,"login:");
+    wrefresh(janelaLogin);
+    refresh();
+
     for(i = 0; (ch = getch()) != '\n'  && i < sizeof(user.login) - 1; i++)
     {
         user.login[i] = ch;
-        printw("%c", ch);
+        wprintw(janelaLogin,"%c", ch);
+        wrefresh(janelaLogin);
         refresh();
     }
 
     user.login[i] = '\0'; //terminação no final da string login
 
-    mvprintw(11,10,"senha:");
+    mvwprintw(janelaLogin,2,1,"senha:");
+    wrefresh(janelaLogin);
+    refresh();
+
     for(i = 0; (ch = getch()) != '\n' && i < sizeof(user.senha) - 1; i++)
     {
         if(ch > 32 && ch < 126)
         {
         user.senha[i] = ch;
-        printw("*");
+        wprintw(janelaLogin,"*");
+        wrefresh(janelaLogin);
         refresh();
         }
         else if (i != 0)
@@ -40,23 +55,39 @@ void cadastro()
             mvprintw(11,10 + i, " ");
         }
         
-    }    
+    }
+        
     user.senha[i] = '\0';
-
+    
+/*
     mvprintw(12,10,"Login: %s\n", user.login);
     mvprintw(13,10,"Senha: %s\n", user.senha);
-    refresh();
-
+    refresh();                                    prova real
+*/
 }
 
-int mainCadastro(){
+int Register(){
 
     initscr(); //inicia o modo curses
     noecho();
     cbreak();
 
-    cadastro();
-    getch();
+    int xmaxstdscr,ymaxstdscr;
+    getmaxyx(stdscr,ymaxstdscr,xmaxstdscr);
+
+
+    WINDOW *janelaLogo;
+    janelaLogo = newwin(7, 50, (ymaxstdscr / 8), (xmaxstdscr / 2.6));
+    refresh();
+
+    WINDOW *janelaLogin;
+    janelaLogin = newwin(4, 30, (ymaxstdscr / 2.1), (xmaxstdscr / 2.3));
+    refresh();
+
+    cadastro(janelaLogo,janelaLogin);
+
+    wclear(janelaLogin);
+    wrefresh(janelaLogin);
     endwin();
     return 0;
 }
